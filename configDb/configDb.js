74f39c97f621a802f934b8ac7d38a106e4fb7a12
phat ho <phatho1508@gmail.com>
2024-09-05 20:03:07 +0700

    test

--- configDb/configDb.js
+++ configDb/configDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 async function connect() {
     try {
 
-        await mongoose.connect('crud-mongodb-shard-00-00.lbb0j.mongodb.net:27017', {
+        await mongoose.connect('mongodb+srv://phatho1508:%3Cdb_password%3E@crud-mongodb.lbb0j.mongodb.net/', {
             seNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
         });
         console.log('db was Connected!');
