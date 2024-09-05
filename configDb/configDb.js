1ede1c8fd1eadc030b709faa0ce7e860a9262d6f
phat ho <phatho1508@gmail.com>
2024-09-05 20:22:19 +0700

    test

--- configDb/configDb.js
+++ configDb/configDb.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 async function connect() {
     try {
 
-        await mongoose.connect('mongodb+srv://phatho1508:%3Cdb_password%3E@crud-mongodb.lbb0j.mongodb.net/', {
-            seNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-        });
+        await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@crud-mongodb.lbb0j.mongodb.net/${process.env.MONGO_DATABASE}`);
         console.log('db was Connected!');
     } catch (error) {
+        console.log(error);
+
         console.log('Connect failure');
 
     }
