e68b925caa050223c80256723f523f3f17ed779c
phat ho <phatho1508@gmail.com>
2024-09-05 17:38:35 +0700

    test

--- configDb/configDb.js
+++ configDb/configDb.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-async function connect(){
+async function connect() {
     try {
-        
-      await  mongoose.connect('mongodb://localhost:27017/crud')
+
+        await mongoose.connect('mongodb://localhost:27017/crud', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000 // Tăng thời gian timeout
+        });
         console.log('db was Connected!');
     } catch (error) {
         console.log('Connect failure');
-        
+
     }
 }
-module.exports={connect};
+module.exports = { connect };
